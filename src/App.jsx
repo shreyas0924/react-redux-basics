12bf93d4f8d8c86b686568fd79621164a49f1a5d
shreyas0924 <shreyasp0924@gmail.com>
2023-06-03 18:15:06 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Login />
+        <Profile />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const Profile = () => {
+  return <div>
+     <h1>Profile Page</h1>
+     <h1>Name : </h1>
+     <h1>Email : </h1>
+     <h1>Age : </h1>
+  </div>
+}
+
+export default Profile
